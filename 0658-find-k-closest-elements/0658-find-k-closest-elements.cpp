a096fca6603d7d239f54ec044a1a6a38716bf39e
ABHOY DAS <126974377+abhoy009@users.noreply.github.com>
2023-08-16 21:13:13 +0530

    Time: 29 ms (91.31%), Space: 31.9 MB (52.01%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoPointer(vector<int>& arr, int k, int x) {
+        
+        vector<int> ans;
+        int l =0;
+        int h = arr.size() -1;
+        
+        while(h - l >= k){
+            if(x - arr[l]> arr[h]-x){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        for(int i=l;i<=h;i++){
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+    
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return twoPointer(arr,k,x);
+    }
+};
