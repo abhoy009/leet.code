e4ec8c11a56a26444ec799a9df7d7102b0868c08
ABHOY DAS <126974377+abhoy009@users.noreply.github.com>
2023-12-10 21:37:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (46.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,8 +7,27 @@ public:
         int ans = recSolve(n-1) + recSolve(n-2);
         return ans;
     }
+    
+    int topDown(int n,vector<int>& dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        //step3: check if ans already exists
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        //step2: replace ans with dp[n]
+        dp[n] = topDown(n-1,dp) + topDown(n-2,dp);
+        return dp[n];
+    }
+    
     int fib(int n){
-        int ans = recSolve(n);
+        // int ans = recSolve(n);
+        // return ans;
+        
+        //step1: create dp array
+        vector<int> dp(n+1, -1);
+        int ans = topDown(n,dp);
         return ans;
     }
 };
